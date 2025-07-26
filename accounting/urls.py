5e1a552ab7416d4f5c8b970258f4c98c4cec7b60
Raihan Ahmed <raihan.tusher@yahoo.com>
2025-07-26 18:57:15 +0600

    accounting expense entry url

--- accounting/urls.py
+++ accounting/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import enter_income
+from .views import enter_income, enter_expense
 
 urlpatterns = [
-    path('', enter_income, name="customer_payment")
+    path('/customer_payment/', enter_income, name="customer_payment"),
+    path('/expense-entry/', enter_expense, name="enter_expense"),
 ]
