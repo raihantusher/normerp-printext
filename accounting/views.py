f7e67c668aca7e865268231d79865390a1afd88b
Raihan Ahmed <raihan.tusher@yahoo.com>
2025-07-26 19:02:37 +0600

    accounting expense entry view

--- accounting/views.py
+++ accounting/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def enter_income(req):
     return render(req, 'backend/accounting/income.html')
+
+
+def enter_expense(req):
+    return render(req, 'backend/accounting/expense_entry.html')
