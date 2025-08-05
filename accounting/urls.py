3d513d27eb6ae8fb0ad70d0abffc72109e134a57
Raihan Ahmed <raihan.tusher@yahoo.com>
2025-08-05 09:30:27 +0600

    new report url

--- accounting/urls.py
+++ accounting/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import enter_income, enter_expense
+from .views import enter_income, enter_expense, income_statement, income_statement_report, balance_sheet
 
 urlpatterns = [
-    path('/customer_payment/', enter_income, name="customer_payment"),
-    path('/expense-entry/', enter_expense, name="enter_expense"),
+    path('customer_payment/', enter_income, name="customer_payment"),
+    path('expense-entry/', enter_expense, name="enter_expense"),
+    path('income-statement/', income_statement, name="income_statement"),
+    path('income-statement-report/', income_statement, name="income_statement_report"),
+    path('balance-sheet/', balance_sheet, name="balance_sheet"),
 ]
