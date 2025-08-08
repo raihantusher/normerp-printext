4f30487ebd19c6a6adc4f77277b93bf3f243e02c
Raihan Ahmed <raihan.tusher@yahoo.com>
2025-08-08 19:25:50 +0600

    make error on main

--- accounting/urls.py
+++ accounting/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('income-statement-report/', income_statement_report, name="income_statement_report"),
     path('balance-sheet/', balance_sheet, name="balance_sheet"),
     path('trial-balance/', trial_balance, name="trial_balance"),
+    -
 ]
