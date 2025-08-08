5115763d2560b5207ba6d0b6b2ce2e3ade80a080
Raihan Ahmed <raihan.tusher@yahoo.com>
2025-08-08 15:31:34 +0600

    income statement and balancesheet working on financial report

--- accounting/views.py
+++ accounting/views.py
@@ -16,15 +16,13 @@ def enter_expense(req):
 
 
 def income_statement(req):
-    income_statement = generate_income_statement(
-        from_date=timezone.make_aware(datetime(2023, 1, 1)),
-        to_date=timezone.make_aware(datetime.now()),
-        method='accrual'
-    )
 
-    return render(req, 'backend/accounting/income_statement.html', income_statement)
+
+    return render(req, 'backend/accounting/income_statement.html')
 
 def income_statement_report(req):
+    frm = req.GET.get('from')  # Get the value of the 'q' parameter
+    to = req.GET.get('to')
     income_statement = generate_income_statement(
         from_date=timezone.make_aware(datetime(2023, 1, 1)),
         to_date=timezone.make_aware(datetime.now()),
@@ -35,5 +33,17 @@ def income_statement_report(req):
 
 
 def balance_sheet(req):
-    return render(req, 'backend/accounting/balance_sheet.html')
+    balance_sheet = generate_balance_sheet(
+        as_of_date=timezone.make_aware(datetime.now())
+    )
+    return render(req, 'backend/accounting/balance_sheet.html', balance_sheet)
 
+
+
+def trial_balance(req):
+    trial_balance_period = generate_trial_balance(
+        from_date=timezone.make_aware(datetime(2023, 1, 1)),
+        to_date=timezone.make_aware(datetime.now())
+    )
+    print(trial_balance_period)
+    return render(req, 'backend/accounting/trial_balance.html', trial_balance_period )
