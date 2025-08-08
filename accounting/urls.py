3c1dc89400b3dcd681b263850e4c51f49a0ce8fd
Raihan Ahmed <raihan.tusher@yahoo.com>
2025-08-08 19:29:28 +0600

    fix errors on feature

--- accounting/urls.py
+++ accounting/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('income-statement-report/', income_statement_report, name="income_statement_report"),
     path('balance-sheet/', balance_sheet, name="balance_sheet"),
     path('trial-balance/', trial_balance, name="trial_balance"),
-    -
 ]
