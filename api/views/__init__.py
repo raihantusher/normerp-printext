07de3837d3adca3b36f3d43970a2af11b0d8262b
Raihan Ahmed <raihan.tusher@yahoo.com>
2025-08-08 15:30:02 +0600

    import AssetList

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,3 +1,8 @@
 from .customer_views import CustomerDetail, CustomerList
+from .asset_views  import AssetList
+__all__ = ['CustomerDetail',
+           'CustomerList',
 
-__all__ = ['CustomerDetail', 'CustomerList']
+           'AssetList'
+
+]
