e8a5740d8663dde3be20cf6d9005c878409d9423
Raihan Ahmed <raihan.tusher@yahoo.com>
2025-08-16 15:46:44 +0600

    report views

--- reports/views.py
+++ reports/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from .pdf_utlis import render_to_pdf
+from datetime import datetime
 
-# Create your views here.
+
+def generate_pdf(request):
+    # Sample data
+    data = {
+        'today': datetime.now().strftime('%Y-%m-%d'),
+        'title': 'My PDF Report',
+        'items': [
+            {'name': 'Product 1', 'quantity': 1, 'price': 9.99},
+            {'name': 'Product 2', 'quantity': 3, 'price': 4.99},
+            {'name': 'Product 3', 'quantity': 2, 'price': 7.99},
+        ]
+    }
+
+    pdf = render_to_pdf('backend/reports/demo_report.html', data)
+
+    if pdf:
+        response = HttpResponse(pdf, content_type='application/pdf')
+        filename = "Invoice_%s.pdf" % data['today']
+        content = "inline; filename=%s" % filename
+        response['Content-Disposition'] = content
+        return response
+    return HttpResponse("Error generating PDF", status=400)
