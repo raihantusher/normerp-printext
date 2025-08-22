c34e2cebcb975fc24a82b9f648e0af1e6d7dd74f
Raihan Ahmed <raihan.tusher@yahoo.com>
2025-08-22 18:38:25 +0600

    add paymentlist in __init__

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,8 +1,11 @@
 from .customer_views import CustomerDetail, CustomerList
 from .asset_views  import AssetList
+from .acc_views import PaymentList
+
 __all__ = ['CustomerDetail',
            'CustomerList',
 
-           'AssetList'
+           'AssetList',
+           'PaymentList'
 
 ]
