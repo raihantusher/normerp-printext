ace7c10d9bcb9675de465651855cc5d511a7c48b
Raihan Ahmed <raihan.tusher@yahoo.com>
2025-08-22 18:40:31 +0600

    serializer  for payments

---
+++ api/serializers/core_serializer.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+
+from core.models import Payment
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = '__all__'
+
+
+
